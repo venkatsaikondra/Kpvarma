87e27a680513da4964d342335a6ac687dfb60963
Venkat Sai Kondra <venkatsaikondra67@gmail.com>
2026-01-20 01:05:58 +0530

    done with animatedCopy

--- components/text/AnimatedCopy.tsx
+++ components/text/AnimatedCopy.tsx
@@ -38,8 +38,69 @@ const AnimatedCopy = ({children,colorInitial='#dddddd',colorAccent='abff02',colo
                 charsClass:"char",
             })
             splitRef.current.push({wordSplit,charSplit});
-        })
-    },{
+        });
+        const allChars=splitRefs.current.flatMap((
+            {charSplit}
+        )=>charSplit.chars);
+        gsap.set(allChars,{color:colorInitial});
+
+        const sheduletFinalTransition=(char,index)=>{
+            if(colorTransitionTimers.current.get(index));
+        }
+        const timer=setTimeout(()=>{
+            if(!completedChars.current.has(index)){
+                gsap.to(char,{
+                    duration:0.1,
+                    ease:"none",
+                    color:"colorFinal",
+                    onComplete:()=>{
+                        completedChars.current.add(index);
+                    }
+                })
+            }
+            colorTransitionTimers.current.delete(index);
+        },100);
+        colorTransitionTimers.current.set(index,timer);
+    },
+    ScrollTrigger.create({
+        trigger:containerRef.current,
+        start:"top:90%",
+        end:"top 10%",
+        scrub:1,
+        onUpdate:(self)=>{
+            const progress=self.progress;
+            const totalChars=allChars.lenth;
+            const isScrollingDown=progress>=lastScrollProgress.current;
+            const currentCharIndex=Math.floor(progress*totalChars);
+
+            allChars.array.forEach((char,index) => {
+                if(!isScrollingDown && index >=currentCharIndex){
+                    if(colorTransitionTimers.current.has(index)){
+                        clearTimeout(colorTransitionTimers.current.get(index));
+                        colorTransitionTimers.current.delete(index);
+                    }
+                    completedChars.current.delete(index);
+                    gsap.set(char,{color:colorInitial});
+                    return;
+                }
+                if(completedChars.current.has(index)){
+                    return;
+                }
+                if(index<=currentCharIndex){
+                    gsap.set(char,{color:colorAccent});
+                    if(!colorTransitionTimers.current.has(index)){
+                        scheduleFinalTransition(char,index);
+                    }
+                    else{
+                        gsap.set(char,{color:colorInitial})
+                    }
+                }
+                lastScrollProgress.current=progress;
+            });
+        },
+    });
+},
+    {
         scope:containerRef;
         dependencies:[colorInitial,colorAccent,colorFinal];
     })
